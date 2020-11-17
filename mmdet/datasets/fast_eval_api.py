2a091abcfa3bef470587b53aec2c4235a89aa8aa
gaurav67890 <gauravsharma01091993@gmail.com>
2020-11-17 22:00:59 +0000

    added dent latest

--- mmdet/datasets/fast_eval_api.py
+++ mmdet/datasets/fast_eval_api.py
@@ -119,7 +119,7 @@ class Params:
         self.catIds = []
         # np.arange causes trouble.  the data point on arange is slightly larger than the true value
         self.iouThrs = np.linspace(.05, 0.95, int(np.round((0.95 - .05) / .05)) + 1, endpoint=True)
-        self.recThrs = np.linspace(.0, 1.00, int(np.round((1.00 - .0) / .01)) + 1, endpoint=True)
+        self.recThrs = np.linspace(.05, 1.00, int(np.round((1.00 - .05) / .01)) + 1, endpoint=True)
         self.maxDets = [1, 10, 100]
         self.areaRng = [[0 ** 2, 1e5 ** 2], [0 ** 2, 32 ** 2], [32 ** 2, 96 ** 2], [96 ** 2, 1e5 ** 2]]
         self.areaRngLbl = ['all', 'small', 'medium', 'large']
