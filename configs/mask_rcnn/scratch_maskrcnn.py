7e4074911c126b068c7fe5b3dfc066701f3cadf7
gaurav67890 <gauravsharma01091993@gmail.com>
2020-11-25 16:38:31 +0000

    fixed scratch path

--- configs/mask_rcnn/scratch_maskrcnn.py
+++ configs/mask_rcnn/scratch_maskrcnn.py
@@ -8,7 +8,7 @@ _base_ = [
 
 classes=['scratch']
 dataset_type = 'CocoDataset'
-data_root = '/mmdetection/data/scratch/'
+data_root = '/mmdetection/data/disk1/scratch_latest/'
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 train_pipeline = [
